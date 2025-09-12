bde40c04215915f437ef8cc89b7b6c83e5ef6c4b
Valery-Holovina <valery.holovina09@gmail.com>
2025-09-12 08:17:00 +0300

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Valery on 12.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
