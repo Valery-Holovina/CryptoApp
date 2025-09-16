d224bd682bba88ae3facbd878199069bf9c79190
Valery-Holovina <valery.holovina09@gmail.com>
2025-09-16 10:05:19 +0300

    #Extension. Double extension convert double into Currency as a String

--- CryptoApp/Extensions/Double.swift
+++ CryptoApp/Extensions/Double.swift
@@ -30,4 +30,18 @@ extension Double{
         formatter.maximumFractionDigits = 6
         return formatter
     }
+    
+    // public to access from our code
+    
+    /// converts a double into a Currency as string with 2-6 decimal places
+    /// ```
+    ///Convert 1234.56 to "$1,234.56"
+    ///Convert 12.3456 to "$12.23456"
+    ///Convert 0.123456 to "$0.123456"
+    /// ```
+
+    func asCurrencyWith6Decimals() -> String{
+        let number = NSNumber(value: self) // current number
+        return currencyFormatter6.string(from: number) ?? "$0.000"
+    }
 }
