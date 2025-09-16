3cb8ee8f4d2cfd78c6825072a9166edfcfe2f295
Valery-Holovina <valery.holovina09@gmail.com>
2025-09-16 09:59:58 +0300

    #Extension DoubleExtension currencyFormatter

---
+++ CryptoApp/Extensions/Double.swift
@@ -0,0 +1,33 @@
+//
+//  Double.swift
+//  CryptoApp
+//
+//  Created by Valery on 16.09.2025.
+//
+
+import Foundation
+
+extension Double{
+    
+    // makes documentation when you hold oprion and click on currencyFormatter6
+    
+    /// converts a double into a Currency with 2-6 decimal places
+    /// ```
+    ///Convert 1234.56 to $1,234.56
+    ///Convert 12.3456 to $12.23456
+    ///Convert 0.123456 to $0.123456
+    /// ```
+    
+    private var currencyFormatter6: NumberFormatter{
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+        //formatter.locale = .current // default value
+        //formatter.currencyCode = "usd" // to change the currency
+        //formatter.currencySymbol = "$" // to change currency symbol
+        
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 6
+        return formatter
+    }
+}
