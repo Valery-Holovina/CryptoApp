d060c3c9a716d9acda65a8afa87ca435243a7844
Valery-Holovina <valery.holovina09@gmail.com>
2025-09-23 09:14:44 +0300

    #Service make downloading reusable

---
+++ CryptoApp/Utilities/NetworkingManager.swift
@@ -0,0 +1,36 @@
+//
+//  NetworkingManager.swift
+//  CryptoApp
+//
+//  Created by Valery on 23.09.2025.
+//
+
+import Foundation
+import Combine
+
+class NetworkingManager{
+    
+    static func download(url: URL) -> AnyPublisher<Data, Error> {
+        
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap { output -> Data in
+                guard let response = output.response as? HTTPURLResponse,
+                      response.statusCode >= 200 && response.statusCode < 300 else{
+                    throw URLError(.badServerResponse)
+                }
+                return output.data
+            }
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher() // convert to  AnyPublisher<Data, Error>
+    }
+    
+    static func handleCompletion(completion: Subscribers.Completion<any Publishers.Decode<AnyPublisher<Data, any Error>, [CoinModel], JSONDecoder>.Failure> ){
+        switch completion{
+        case .finished:
+            break
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+    }
+}
