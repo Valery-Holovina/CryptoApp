764edea9f2b77292bca4e9b973e06519c6503b21
Valery-Holovina <valery.holovina09@gmail.com>
2025-09-23 09:21:05 +0300

    #Service handleURLResponse

--- CryptoApp/Utilities/NetworkingManager.swift
+++ CryptoApp/Utilities/NetworkingManager.swift
@@ -14,15 +14,17 @@ class NetworkingManager{
         
         return URLSession.shared.dataTaskPublisher(for: url)
             .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap { output -> Data in
-                guard let response = output.response as? HTTPURLResponse,
-                      response.statusCode >= 200 && response.statusCode < 300 else{
-                    throw URLError(.badServerResponse)
-                }
-                return output.data
-            }
+            .tryMap({ try handleURLResponse(output: $0)})
             .receive(on: DispatchQueue.main)
             .eraseToAnyPublisher() // convert to  AnyPublisher<Data, Error>
+    }
+    
+    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output) throws -> Data{
+        guard let response = output.response as? HTTPURLResponse,
+              response.statusCode >= 200 && response.statusCode < 300 else{
+            throw URLError(.badServerResponse)
+        }
+        return output.data
     }
     
     static func handleCompletion(completion: Subscribers.Completion<any Publishers.Decode<AnyPublisher<Data, any Error>, [CoinModel], JSONDecoder>.Failure> ){
