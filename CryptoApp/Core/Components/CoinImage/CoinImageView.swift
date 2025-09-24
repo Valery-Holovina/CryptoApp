7e65a1ced19cadc7afa3e7babd12276fa1cd60e3
Valery-Holovina <valery.holovina09@gmail.com>
2025-09-24 10:35:06 +0300

    #Service&&View Downloading coin Images

---
+++ CryptoApp/Core/Components/CoinImage/CoinImageView.swift
@@ -0,0 +1,37 @@
+//
+//  CoinImageView.swift
+//  CryptoApp
+//
+//  Created by Valery on 24.09.2025.
+//
+
+import SwiftUI
+
+
+struct CoinImageView: View {
+    
+    @StateObject var vm: CoinImageViewModel
+    
+    init(coin: CoinModel) {
+        _vm = StateObject(wrappedValue: CoinImageViewModel(coin: coin))
+    }
+    
+    var body: some View {
+        ZStack{
+            if let image = vm.image{
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            }else if vm.isLoading{
+                ProgressView()
+            }else{
+                Image(systemName: "questionmark")
+                    .foregroundStyle(Color.theme.secondaryText)
+            }
+        }
+    }
+}
+
+#Preview {
+    CoinImageView(coin: DeveloperPreview.instance.coin)
+}
