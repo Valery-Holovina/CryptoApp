b1937b05dd278d2869c3a34b4d3c23cbbecae387
Valery-Holovina <valery.holovina09@gmail.com>
2025-10-06 17:30:57 +0300

    #Clean PortfolioView clean

--- CryptoApp/Core/Home/View/PortfolioView.swift
+++ CryptoApp/Core/Home/View/PortfolioView.swift
@@ -18,28 +18,14 @@ struct PortfolioView: View {
             ScrollView{
                 VStack(alignment: .leading, spacing: 0) {
                     SearchBarView(searchText: $vm.searchText)
+                    coinLogoList
                     
-                    ScrollView(.horizontal) {
-                        LazyHStack( spacing: 10) {
-                            ForEach(vm.allCoins) { coin in
-                                CoinLogoView(coin: coin)
-                                    .frame(width: 75)
-                                    .padding(4)
-                                    .onTapGesture {
-                                        withAnimation(.easeInOut) {
-                                            selectedCoin = coin
-                                        }
-                                    }
-                                    .background(
-                                        RoundedRectangle(cornerRadius: 10)
-                                            .stroke(
-                                                selectedCoin?.id == coin.id ?
-                                                Color.theme.green : Color.clear, lineWidth: 1)
-                                    )
+                    if selectedCoin != nil{
+                        VStack(spacing: 20) {
+                            HStack{
+                                Text("Current price of \(selectedCoin?.symbol.uppercased() ?? "")")
                             }
                         }
-                        .padding(.vertical, 4)
-                        .padding(.leading)
                     }
                 }
             }
@@ -61,4 +47,33 @@ struct PortfolioView: View {
 #Preview {
     PortfolioView()
         .environmentObject(DeveloperPreview.instance.homeVM)
+}
+
+extension PortfolioView{
+    
+    private var coinLogoList : some View{
+        ScrollView(.horizontal) {
+            LazyHStack( spacing: 10) {
+                ForEach(vm.allCoins) { coin in
+                    CoinLogoView(coin: coin)
+                        .frame(width: 75)
+                        .padding(4)
+                        .onTapGesture {
+                            withAnimation(.easeInOut) {
+                                selectedCoin = coin
+                            }
+                        }
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(
+                                    selectedCoin?.id == coin.id ?
+                                    Color.theme.green : Color.clear, lineWidth: 1)
+                        )
+                }
+            }
+            .padding(.vertical, 4)
+            .padding(.leading)
+        }
+
+    }
 }
