d4aa67feb3fcd0f454080e94caeb88bddc899357
Valery-Holovina <valery.holovina09@gmail.com>
2025-10-25 12:08:17 +0300

    #View&&ViewModel View setup and overview in ViewModel

--- CryptoApp/Core/Details/ViewModels/DetailViewModel.swift
+++ CryptoApp/Core/Details/ViewModels/DetailViewModel.swift
@@ -10,19 +10,51 @@ import Combine
 
 class DetailViewModel: ObservableObject{
     
+    @Published var overviewStatistics: [StatisticModel] = []
+    @Published var additionalStatistics: [StatisticModel] = []
+    
+    @Published var coin : CoinModel
+    
     private let coinDetailService: CoinDetailDataService
     private var cancellables = Set<AnyCancellable>()
     
+    
     init(coin: CoinModel) {
+        self.coin = coin
         self.coinDetailService = CoinDetailDataService(coin: coin)
         self.addSubscribers()
     }
     
     private func addSubscribers(){
         coinDetailService.$coinDetails
-            .sink { returnedCoinDetails in
+            .combineLatest($coin)
+            .map({ coinDetailModel, coinModel -> (overview: [StatisticModel], additional: [StatisticModel])in
+                
+                //Overview
+                let price = coinModel.currentPrice.asCurrencyWith6Decimals()
+                let priceChange = coinModel.priceChangePercentage24H
+                let priceStat = StatisticModel(title: "Current Price", value: price, percentageChange: priceChange)
+                
+                let marketCap = "$" + (coinModel.marketCap?.formattedWithAbbreviations() ?? "")
+                let marketCapChange = coinModel.marketCapChangePercentage24H
+                let marketCapStat = StatisticModel(title: "Market Capitalization", value: marketCap, percentageChange: marketCapChange)
+                
+                
+                let rank = "\(coinModel.rank)"
+                let rankStat = StatisticModel(title: "Rank", value: rank)
+                
+                let volume = "$" + (coinModel.totalVolume?.formattedWithAbbreviations() ?? "")
+                let volumeStat = StatisticModel(title: "Volume", value: volume)
+                
+                let overviewArray: [StatisticModel] = [priceStat,marketCapStat,rankStat,volumeStat]
+                
+                
+                return (overviewArray,[])
+            })
+            .sink { returnedArrays in
                 print("Received coin detail data")
-                print(returnedCoinDetails)
+                print(returnedArrays.overview)
+                print(returnedArrays.additional)
             }
             .store(in: &cancellables)
     }
--- CryptoApp/Core/Details/Views/DeatilView.swift
+++ CryptoApp/Core/Details/Views/DeatilView.swift
@@ -25,19 +25,69 @@ struct DetailLoadingView: View {
 
 struct DeatilView: View {
     
-    @StateObject var vm : DetailViewModel
+    @StateObject private var vm : DetailViewModel
+    
+    private let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    private let spacing: CGFloat = 30
         
     init(coin: CoinModel) {
         _vm = StateObject(wrappedValue: DetailViewModel(coin: coin))
-        print("Initializing Detail View for \(String(describing: coin.name))")
     }
     
     var body: some View {
-        Text("Hi")
+        ScrollView{
+            VStack(spacing: 20){
+                Text("")
+                    .frame(height: 150)
+                Text("Overview")
+                    .font(.title)
+                    .bold()
+                    .foregroundStyle(Color.theme.accent)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Divider()
+                LazyVGrid(
+                    columns: columns,
+                    alignment: .leading,
+                    spacing: spacing,
+                    pinnedViews: []) {
+                        ForEach(0..<6) { _ in
+                            StatisticView(stat: StatisticModel(title: "Title", value: "Value"))
+                        }
+                    }
+                
+                
+                Text("Additional Details")
+                    .font(.title)
+                    .bold()
+                    .foregroundStyle(Color.theme.accent)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Divider()
+                
+                LazyVGrid(
+                    columns: columns,
+                    alignment: .leading,
+                    spacing: spacing,
+                    pinnedViews: []) {
+                        ForEach(0..<6) { _ in
+                            StatisticView(stat: StatisticModel(title: "Title", value: "Value"))
+                        }
+                    }
+
+               
+            }
+            .padding()
+            
+        }
+        .navigationTitle(vm.coin.name)
         
     }
 }
 
 #Preview {
-    DeatilView(coin: DeveloperPreview.instance.coin)
+    NavigationStack{
+        DeatilView(coin: DeveloperPreview.instance.coin)
+    }
 }
