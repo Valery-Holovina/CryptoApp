2dcd4c4d1506d2c488a836b4ea702b1a42b562c7
Valery-Holovina <valery.holovina09@gmail.com>
2025-11-05 10:15:55 +0200

    #View add Settings to HomeView

--- CryptoApp/Core/Home/View/HomeView.swift
+++ CryptoApp/Core/Home/View/HomeView.swift
@@ -14,6 +14,7 @@ struct HomeView: View {
     
     @State private var showPortfolio: Bool = false // animate right
     @State private var showPortfolioView : Bool = false // new sheet
+    @State private var showSettingsView: Bool = false // new sheet
     
     @State private var selectedCoin : CoinModel? = nil
     @State private var showDetailView: Bool = false
@@ -52,6 +53,9 @@ struct HomeView: View {
              
                 Spacer(minLength: 0)
             }
+            .sheet(isPresented: $showSettingsView) {
+                SettingsView()
+            }
         }
      
             .navigationDestination(isPresented: $showDetailView) {
@@ -82,6 +86,8 @@ extension HomeView{
                 .onTapGesture {
                     if showPortfolio{
                         showPortfolioView.toggle()
+                    }else{
+                        showSettingsView.toggle()
                     }
                 }
                 .background(
