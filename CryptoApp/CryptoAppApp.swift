abc4a82156e8a95eb43e1e49deb0c14b925c5416
Valery-Holovina <valery.holovina09@gmail.com>
2025-11-06 09:39:28 +0200

    #LaunchView

--- CryptoApp/CryptoAppApp.swift
+++ CryptoApp/CryptoAppApp.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct CryptoAppApp: App {
     
     @StateObject private var vm = HomeViewModel()
+    @State private var showLaunchView: Bool = true
     
     init() {
         UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
@@ -18,12 +19,20 @@ struct CryptoAppApp: App {
     }
     var body: some Scene {
         WindowGroup {
-            NavigationStack{
-                HomeView()
-                    .toolbarVisibility(.hidden, for: .navigationBar)
+            ZStack{
+                NavigationStack{
+                    HomeView()
+                        .toolbarVisibility(.hidden, for: .navigationBar)
+                }
+                .environmentObject(vm)
+                ZStack{
+                    if showLaunchView{
+                        LaunchView(showLaunchView: $showLaunchView)
+                            .transition(.move(edge: .leading))
+                    }
+                }
+                .zIndex(2.0)
             }
-            .environmentObject(vm)
-          
         }
     }
 }
